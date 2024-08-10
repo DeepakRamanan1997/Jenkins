f4ad96ee6f1b65d6a62be14261e3a6c1e4920ce1
DeepakRamanan1997 <131782451+DeepakRamanan1997@users.noreply.github.com>
2024-08-10 16:30:10 +0530

    Update SimpleJavaClass.java

--- Freestyle/src/main/java/com/example/SimpleJavaClass.java
+++ Freestyle/src/main/java/com/example/SimpleJavaClass.java
@@ -1,7 +1,7 @@
 package com.example;
 
-public class SimpleJavaClass {
-  public static void main(String[] args) {
-    System.out.println("Hello, World!");
-  }
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
